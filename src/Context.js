ef2ff5bf8dfa704c9fdc6a542372f0ad6c5240f6
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-01-17 15:51:36 -0800

    Creating context

---
+++ src/Context.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Context = React.createContext();
+
+function ContextProvider(props) {
+  return <Context.Provider value="">{props.children}</Context.Provider>;
+}
+
+export { Context, ContextProvider };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { Context, ContextProvider } from "./Context";
 import App from "./App";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ContextProvider>,
   document.getElementById("root")
 );
