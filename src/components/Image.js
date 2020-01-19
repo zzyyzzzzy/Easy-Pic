46276bc35722ec9e4403aad4f436e6bc37e84c5a
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-01-18 23:44:24 -0800

    add proptypes forimage component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../Context";
+import PropTypes from "prop-types";
 
 function Image({ className, img }) {
   const [hovered, setHovered] = useState(false);
@@ -25,5 +26,14 @@ function Image({ className, img }) {
     </div>
   );
 }
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool
+  })
+};
 
 export default Image;
