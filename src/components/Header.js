8f09db698c78167c2f699c5649934a405a6d800e
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2020-01-19 16:54:56 -0800

    finsh part of the cart functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context";
 
 function Header() {
+  const { cartItems } = useContext(Context);
+  const cartClassName =
+    cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line";
   return (
     <header>
       <h2>
         <Link to="/">Pic Some</Link>
       </h2>
       <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        <i className={`${cartClassName} ri-fw`} ri-2x></i>
       </Link>
     </header>
   );
